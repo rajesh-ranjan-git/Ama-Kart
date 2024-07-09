1f05d472e0201945a7ed2746559f1394cb799b8b
Rajesh <rajeshranjan8271@gmail.com>
2024-07-10 01:20:15 +0530

    Static Cart modal implemented

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -37,14 +37,42 @@ const index = ({ cartItemsCounter }) => {
           <div className="checkout-modal">
             <h2>Checkout Modal</h2>
             <div className="checkout-modal_list">
-              <div className="empty-cart">
-                Please add something on your cart!
-              </div>
+              {cartItemsCounter > 0 ? (
+                <div className="checkout-modal_list-item">
+                  <div className="img-wrap">
+                    <img
+                      src={"/assets/placeholder.png"}
+                      alt="img-cart-item"
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="information">
+                    <div>
+                      <h4>Title of the Product</h4>
+                      <div className="pricing">
+                        <span>2000</span>
+                        <small>
+                          <strike>2500</strike>
+                        </small>
+                      </div>
+                    </div>
+                    <div className="cart-addon cart-addon__modal">
+                      <button>-</button>
+                      <span className="counter">{0}</span>
+                      <button>+</button>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="empty-cart">
+                  Please add something on your cart!
+                </div>
+              )}
             </div>
             {cartItemsCounter > 0 && (
               <div className="checkout-modal_footer">
                 <div className="totalAmount">
-                  <h4>Total Amout: </h4>
+                  <h4>Total Amount: </h4>
                   <h4>2000 INR</h4>
                 </div>
                 <button>Order Now</button>
