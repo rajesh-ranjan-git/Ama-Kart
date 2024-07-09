660381021c56187132fe814a75c2b26bd8a2a239
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-07-09 14:15:02 +0530

    All bugs fixed for Modal

--- src/components/Products/ListItem.jsx
+++ src/components/Products/ListItem.jsx
@@ -57,8 +57,8 @@ const ListItem = ({ data }) => {
             </div>
           )}
         </div>
-        {showModal && <Modal onClose={handleModal} />}
       </div>
+      {showModal && <Modal onClose={handleModal} />}
     </Fragment>
   );
 };
