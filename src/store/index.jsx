02202d8b142361402d60d9c21647e2e418af4afa
Rajesh <rajeshranjan8271@gmail.com>
2024-07-11 02:03:58 +0530

    Central store created

---
+++ src/store/index.jsx
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+import mainReducer from "../reducers";
+
+const store = createStore(
+  mainReducer,
+  {
+    items: [],
+    totalAmount: 0,
+  },
+  applyMiddleware(thunk)
+);
+
+export default store;
