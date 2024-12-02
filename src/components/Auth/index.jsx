23b8d028b4816484a8ac34c1fb61f36caefc3178
Rajesh <rajeshranjan8271@gmail.com>
2024-12-03 02:00:36 +0530

    Added Firebase authentication

--- src/components/Auth/index.jsx
+++ src/components/Auth/index.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useLocation, useParams } from "react-router";
+import { useLocation } from "react-router";
 import { NavLink } from "react-router-dom";
+import Loader from "../UI/Loader";
 
 const AuthIndex = () => {
+  const [loader, setLoader] = useState(false);
   const [details, setDetails] = useState({
     email: "",
     password: "",
@@ -24,14 +26,16 @@ const AuthIndex = () => {
     console.log(details);
 
     if (location.pathname === "/signup") {
-      signupWithEmailAndPassword();
+      signUpWithEmailAndPassword();
     }
   };
 
-  const signupWithEmailAndPassword = async () => {
+  const signUpWithEmailAndPassword = async () => {
+    setLoader(true);
     try {
+      console.log("This is from signupWithEmailAndPassword");
       const response = await axios.post(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=[3AIzaSyDTqbgeII96O1HO3ismm_xgRhq8lN4WH6U]`,
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDTqbgeII96O1HO3ismm_xgRhq8lN4WH6U`,
         {
           email: details.email,
           password: details.password,
@@ -40,50 +44,55 @@ const AuthIndex = () => {
       );
       console.log(response);
     } catch (error) {
-      console.log(error);
+      console.log(error.response);
+    } finally {
+      setLoader(false);
     }
   };
 
   return (
-    <div className="auth-container">
-      <div className="auth-container--box">
-        <div className="tab-selector">
-          <NavLink to={"/login"}>
-            <h1>Login</h1>
-          </NavLink>
-          <NavLink to={"/signup"}>
-            <h1>Signup</h1>
-          </NavLink>
-        </div>
-        <form autoComplete="off" onSubmit={handleSubmission}>
-          <div className="input-wrap">
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              name="email"
-              placeholder="Enter Email..."
-              value={details.email}
-              onChange={handleInput}
-            />
+    <>
+      <div className="auth-container">
+        <div className="auth-container--box">
+          <div className="tab-selector">
+            <NavLink to={"/login"}>
+              <h1>Login</h1>
+            </NavLink>
+            <NavLink to={"/signup"}>
+              <h1>Signup</h1>
+            </NavLink>
           </div>
-          <div className="input-wrap">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Enter Password..."
-              value={details.password}
-              onChange={handleInput}
-            />
-          </div>
-          <div className="button-wrap">
-            <div className="login-btn" onClick={handleSubmission}>
-              {location.pathname === "/login" ? "Login" : "Signup"}
+          <form autoComplete="off" onSubmit={handleSubmission} method="POST">
+            <div className="input-wrap">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                name="email"
+                placeholder="Enter Email..."
+                value={details.email}
+                onChange={handleInput}
+              />
             </div>
-          </div>
-        </form>
+            <div className="input-wrap">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter Password..."
+                value={details.password}
+                onChange={handleInput}
+              />
+            </div>
+            <div className="button-wrap">
+              <button className="login-btn">
+                {location.pathname === "/login" ? "Login" : "Signup"}
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+      {loader && <Loader />}
+    </>
   );
 };
 
